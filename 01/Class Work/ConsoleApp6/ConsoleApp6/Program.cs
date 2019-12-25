d6489849957f94c74a87601634139cf6517f5503
anton29141@gmail.com <[github_email]>
2019-12-25 22:09:12 +0300

    Initial commit

---
+++ 01/Class Work/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            String name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine($"Досвидания, {name}!");
+            Console.ReadKey();
+        }
+    }
+}
