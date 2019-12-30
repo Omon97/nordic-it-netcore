59a01c11a092dea319062b6cb9195676bb589603
omon97 <Baltaevomon@gmail.com>
2019-12-30 14:14:14 +0300

    Home work of the 1st lesson added

---
+++ 01/HomeWork/L01_HomeWork/L01_HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L01_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
