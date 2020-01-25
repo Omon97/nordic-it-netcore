7fa423eee59ce3effb30768350020c0994c8976d
Omon97 <omon_97@mail.ru>
2020-01-25 14:10:29 +0300

    ClassWork 06 added

---
+++ 07/ClassWork 07.2/ClassWork 07.2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassWork_07._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку:");
+            string text = Console.ReadLine();
+            Console.Write("Введите подстроку для поиска: ");
+            string substring = Console.ReadLine();
+
+            int index = 0;
+            string indexes = string.Empty;
+            do
+            {
+                index = text.IndexOf(substring, startIndex: 0);
+                if (index >= 0) 
+                    break;
+                {
+                    indexes += index + " ";
+                    index++;
+                }
+            } while (true);
+            
+            if(string.IsNullOrEmpty(indexes))
+            {
+                Console.WriteLine("Искомая подстрока не найдена");
+            }
+            else
+            {
+                indexes = indexes.Substring(startIndex: 0, length:indexes.Length - 1);
+                Console.WriteLine("Индексы подстрок в строке: " + indexes.Replace(oldValue: " ", newValue: ", "));
+            }
+
+
+
+            //Console.WriteLine("Введите строку:");
+            //string text = Console.ReadLine();
+            //Console.WriteLine("Введите подстроку для поиска:");
+            //string search = Console.ReadLine();
+            //int index = text.IndexOf(search);
+            //if (index < 0)
+            //{
+            //    Console.WriteLine("Искомая строка не найдена");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Индекс подстроки в строке:" + text.IndexOf(search));
+            //}
+        }   
+    }
+}
---
+++ 07/ClassWork 07/ClassWork 07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassWork_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("Enter first value:");
+            a = Console.ReadLine();
+            double c;
+            double.TryParse(a, out c);
+            
+            Console.WriteLine("Enter second value:");
+            a = Console.ReadLine();
+            double d;
+            double.TryParse(a, out d);
+            double sum = c * d;
+            string result1 = c + " * " + d + " = " + sum;
+            Console.WriteLine(result1);
+            string result2 = String.Format("{0:#.##} + {1:#.##} = {2}", c, d, sum);
+            Console.WriteLine(result2);
+            string result3 = $"{c} - {d} = {sum}";
+            Console.WriteLine(result3);
+        }
+    }
+}
